68e41de1e6a1ce073b1663abf412bc0d1e39b675
Administrator <gitlab_admin_19636a@example.com>
2025-12-03 16:04:37 +0000

    Update file test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -13,7 +13,7 @@ def client():
 def test_get_index(client):
     rv = client.get("/")
     assert rv.status_code == 200
-    assert b"Текстовый редактор" in rv.data
+    assert b"Текстовый редактор" in rv.data.decode()
 
 def test_save_txt(client):
     data = {"editor": "Привет, мир!", "format": "txt"}
